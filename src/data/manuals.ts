6821a5c4bd4c9754d2702d9843cc205a9aed86b2
poetrainy <manamisato2001@gmail.com>
2024-02-09 22:09:02 +0900

    add: ケトル、炊飯器、電子レンジを追加

--- src/data/manuals.ts
+++ src/data/manuals.ts
@@ -1,9 +1,28 @@
 import IconWasher from "~/components/IconWasher";
+import IconRiceCooker from "~/components/IconRiceCooker";
+import IconKettle from "~/components/IconKettle";
+import IconMicrowave from "~/components/IconMicrowave";
 import IconSpeaker from "~/components/IconSpeaker";
 import MANUAL_WASHER from "~/assets/manuals/washer.pdf";
+import MANUAL_RICE_COOKER from "~/assets/manuals/rice-cooker.pdf";
+import MANUAL_MICROWAVE from "~/assets/manuals/microwave.pdf";
+import MANUAL_KETTLE from "~/assets/manuals/kettle.pdf";
 import MANUAL_SPEAKER from "~/assets/manuals/speaker.pdf";
 
 export const MANUALS = [
-  { label: "洗濯機", file: MANUAL_WASHER, icon: IconWasher },
-  { label: "Anker SoundCore 2", file: MANUAL_SPEAKER, icon: IconSpeaker },
+  {
+    type: "家電",
+    manuals: [
+      { label: "洗濯機", file: MANUAL_WASHER, icon: IconWasher },
+      { label: "炊飯器", file: MANUAL_RICE_COOKER, icon: IconRiceCooker },
+      { label: "電子レンジ", file: MANUAL_MICROWAVE, icon: IconMicrowave },
+      { label: "ケトル", file: MANUAL_KETTLE, icon: IconKettle },
+    ],
+  },
+  {
+    type: "機器",
+    manuals: [
+      { label: "Anker SoundCore 2", file: MANUAL_SPEAKER, icon: IconSpeaker },
+    ],
+  },
 ];
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,59 @@
-import { Center, Flex, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { MANUALS } from "~/data/manuals";
 
 const Home = () => {
   return (
-    <Flex
-      alignItems="flex-start"
-      flexWrap="wrap"
-      gap="16px"
-      w="100%"
-      maxW="400px"
-      p="32px 16px"
-      m="0 auto"
-    >
-      {MANUALS.map((manual) => (
-        <Center
-          as={Link}
-          key={manual.label}
-          href={manual.file}
-          isExternal
-          flexDir="column"
-          gap="6px"
-          w="calc(50% - 8px)"
-          p="12px 0 0"
-          bg="white"
-          boxShadow="0 0 10px rgba(0, 0, 0, 0.1)"
-          rounded="24px"
-          aspectRatio="1"
-        >
-          <manual.icon width="52%" height="52%" fill="#086776" />
-          <Text as="span" color="primary.700" fontWeight="bold">
-            {manual.label}
-          </Text>
-        </Center>
+    <VStack gap="24px" w="100%" maxW="400px" m="0 auto" p="32px 16px">
+      {MANUALS.map(({ type, manuals }) => (
+        <VStack key={type} alignItems="stretch" gap="16px" w="100%">
+          <Heading as="h2" fontSize="16px" color="primary.800">
+            {type}
+          </Heading>
+          <Flex
+            alignItems="flex-start"
+            flexWrap="wrap"
+            gap="16px"
+            w="100%"
+            m="0 auto"
+          >
+            {manuals.map((manual) => (
+              <Center
+                as={Link}
+                key={manual.label}
+                href={manual.file}
+                isExternal
+                flexDir="column"
+                gap="6px"
+                w="calc(50% - 8px)"
+                p="12px 0 0"
+                bg="white"
+                boxShadow="0 0 10px rgba(0, 0, 0, 0.1)"
+                rounded="24px"
+                aspectRatio="1"
+              >
+                <Box
+                  as={manual.icon}
+                  boxSize="52%"
+                  fill="#086776"
+                  objectFit="contain"
+                />
+                <Text as="span" color="primary.700" fontWeight="bold">
+                  {manual.label}
+                </Text>
+              </Center>
+            ))}
+          </Flex>
+        </VStack>
       ))}
-    </Flex>
+    </VStack>
   );
 };
 
